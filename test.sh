0ff673d3f27c0fa8fd7f50d811230404efb34e28
k-kohey <kawagutsuchi@gmail.com>
2024-12-01 16:59:46 +0900

    トークナイザを導入

--- test.sh
+++ test.sh
@@ -9,14 +9,15 @@ assert() {
   actual="$?"
 
   if [ "$actual" = "$expected" ]; then
-    echo "$input => $actual"
+    echo "✅️ $input => $actual"
   else
-    echo "$input => $expected expected, but got $actual"
+    echo "❌️ $input => $expected expected, but got $actual"
     exit 1
   fi
 }
 
 assert 0 0
 assert 42 42
+assert 41 " 12 + 34 - 5 "
 
 echo OK
